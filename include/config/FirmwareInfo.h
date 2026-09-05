ef252b4af1e70e043b8d892a4fd7e0a582d4b703
VooxergAlt2 <vooxerg@gmail.com>
2026-09-06 01:54:14 +0300

    feat(config): centralize firmware identity and protocol version

---
+++ include/config/FirmwareInfo.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstdint>
+
+namespace ambilight::config {
+
+inline constexpr const char kFirmwareName[] =
+    "ambilight-c6";
+
+inline constexpr const char kFirmwareVersion[] =
+    "0.34.0-dev";
+
+inline constexpr const char kFirmwareTarget[] =
+    "ESP32-C6";
+
+inline constexpr std::uint16_t kDevelopmentStage =
+    34;
+
+inline constexpr std::uint16_t kSerialProtocolVersion =
+    1;
+
+} // namespace ambilight::config
