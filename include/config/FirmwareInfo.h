0d20f7ee91ea5b503729f68f9463d135f1b2a401
VooxergAlt2 <vooxerg@gmail.com>
2026-09-06 20:48:29 +0300

    chore(firmware): identify Stage 37 pixel-mask build

--- include/config/FirmwareInfo.h
+++ include/config/FirmwareInfo.h
@@ -8,13 +8,13 @@ inline constexpr const char kFirmwareName[] =
     "ambilight-c6";
 
 inline constexpr const char kFirmwareVersion[] =
-    "0.36.0-dev";
+    "0.37.0-dev";
 
 inline constexpr const char kFirmwareTarget[] =
     "ESP32-C6";
 
 inline constexpr std::uint16_t kDevelopmentStage =
-    36;
+    37;
 
 inline constexpr std::uint16_t kSerialProtocolVersion =
     1;
