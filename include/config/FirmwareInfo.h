b8de76c8db6a310ec5b589643a457fea2ebdcc7f
VooxergAlt2 <vooxerg@gmail.com>
2026-09-06 22:11:02 +0300

    chore(firmware): identify Stage 38 commissioning protocol

--- include/config/FirmwareInfo.h
+++ include/config/FirmwareInfo.h
@@ -8,15 +8,15 @@ inline constexpr const char kFirmwareName[] =
     "ambilight-c6";
 
 inline constexpr const char kFirmwareVersion[] =
-    "0.37.0-dev";
+    "0.38.0-dev";
 
 inline constexpr const char kFirmwareTarget[] =
     "ESP32-C6";
 
 inline constexpr std::uint16_t kDevelopmentStage =
-    37;
+    38;
 
 inline constexpr std::uint16_t kSerialProtocolVersion =
-    1;
+    2;
 
 } // namespace ambilight::config
