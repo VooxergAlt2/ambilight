d4ef5034867e595f496e1efc57cef3327a03ca65
VooxergAlt2 <vooxerg@gmail.com>
2026-09-06 20:47:12 +0300

    test(serial): cover disabled-pixel line framing

--- test/test_serial_command_parser/test_main.cpp
+++ test/test_serial_command_parser/test_main.cpp
@@ -269,6 +269,12 @@ void test_empty_line_commands_emit_status_events() {
         SerialCommandKind::LedMap,
         "");
 
+    parser.feed('d');
+    assertEvent(
+        parser.feed('\n'),
+        SerialCommandKind::LedPixelMask,
+        "");
+
     parser.feed('b');
     assertEvent(
         parser.feed('\n'),
@@ -280,6 +286,20 @@ void test_empty_line_commands_emit_status_events() {
         parser.feed('\n'),
         SerialCommandKind::Factory,
         "");
+}
+
+void test_disabled_pixel_line_payload_is_emitted() {
+    SerialCommandParser parser;
+
+    const auto event =
+        feedText(
+            parser,
+            "d-,12,-,0\r");
+
+    assertEvent(
+        event,
+        SerialCommandKind::LedPixelMask,
+        "-,12,-,0");
 }
 
 void test_line_command_prefix_is_not_reinterpreted_inside_payload() {
@@ -476,6 +496,7 @@ int main(int, char**) {
     RUN_TEST(test_wifi_line_payload_is_emitted_on_enter);
     RUN_TEST(test_wifi_accepts_full_32_plus_63_character_payload);
     RUN_TEST(test_empty_line_commands_emit_status_events);
+    RUN_TEST(test_disabled_pixel_line_payload_is_emitted);
     RUN_TEST(test_line_command_prefix_is_not_reinterpreted_inside_payload);
     RUN_TEST(test_brightness_accepts_three_digits);
     RUN_TEST(test_fourth_brightness_digit_errors_and_discards_to_eol);
