b00dc4fa6d149042937df2eeeb1275cced81b088
VooxergAlt2 <vooxerg@gmail.com>
2026-09-07 16:29:36 +0300

    refactor(led): use shared physical frame write view

--- include/led/LedEngine.h
+++ include/led/LedEngine.h
@@ -10,53 +10,9 @@
 #include "config/BoardConfig.h"
 #include "core/PerformanceMetric.h"
 #include "core/RgbFrame.h"
+#include "led/LedFrameWriteView.h"
 
 namespace ambilight {
-
-struct LedLaneWriteView {
-    std::uint8_t* grb = nullptr;
-    std::uint16_t pixelCount = 0;
-
-    bool valid() const {
-        return
-            grb != nullptr &&
-            pixelCount > 0;
-    }
-
-    void writeUnchecked(
-        std::uint16_t physicalIndex,
-        const Rgb8& color) const {
-
-        std::uint8_t* pixel =
-            grb +
-            static_cast<std::size_t>(
-                physicalIndex) *
-            3U;
-
-        pixel[0] = color.g;
-        pixel[1] = color.r;
-        pixel[2] = color.b;
-    }
-};
-
-struct LedFrameWriteView {
-    std::array<
-        LedLaneWriteView,
-        config::kParlioLaneCount>
-        lane{};
-
-    bool valid() const {
-        for (const auto& current :
-             lane) {
-
-            if (!current.valid()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-};
 
 // Thin owner of the ESP32-C6 PARLIO hardware.
 //
