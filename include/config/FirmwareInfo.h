a1acbfd1dc1fb13c7188ca392c041295c1a67b53
VooxergAlt2 <vooxerg@gmail.com>
2026-09-09 23:41:38 +0300

    chore: bump firmware identity for Stage 45

--- include/config/FirmwareInfo.h
+++ include/config/FirmwareInfo.h
@@ -8,13 +8,13 @@ inline constexpr const char kFirmwareName[] =
     "ambilight-c6";
 
 inline constexpr const char kFirmwareVersion[] =
-    "0.44.1-dev";
+    "0.45.0-dev";
 
 inline constexpr const char kFirmwareTarget[] =
     "ESP32-C6";
 
 inline constexpr std::uint16_t kDevelopmentStage =
-    44;
+    45;
 
 inline constexpr std::uint16_t kSerialProtocolVersion =
     2;
